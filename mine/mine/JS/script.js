a96f121cc73c4c5a0661d63ea3e3095a745afe59
HONRAY <116732100+HONRAY001@users.noreply.github.com>
2023-05-08 00:09:18 +0100

    Add files via upload

---
+++ mine/mine/JS/script.js
@@ -0,0 +1,7 @@
+/* ************************* typing animation ***************************** */
+var typed = new Typed(".typing",{
+    strings:["","Software Engineer","Graphic Designer","Web Designer","Video Editor","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
